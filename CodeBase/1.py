ce4040731ae3b57233d7bb9ac4839fbf7e568c3b
Tin Phan <thanhtin281@gmail.com>
2025-09-14 10:44:48 +0700

    restructure

---
+++ CodeBase/1.py
@@ -0,0 +1,7 @@
+from pwn import xor
+
+text = b"label"
+key = 13
+
+result = xor(text, key)
+print(b"crypto{" + result + b"}")
---
+++ CodeBase/bytes_and_big_integers.py
@@ -0,0 +1,5 @@
+from Crypto.Util.number import *
+
+n = 11515195063862318899931685488813747395775516287289682636499965282714637259206269
+message = long_to_bytes(n)
+print(message.decode())
